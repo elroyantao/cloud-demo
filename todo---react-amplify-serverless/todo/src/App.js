e4a42810f879d6b30b0e24b66f777716eb804c1d
Elroy Antao <elroyantao@gmail.com>
2020-12-05 00:58:26 +0000

    add mutations and subscriptions

--- todo---react-amplify-serverless/todo/src/App.js
+++ todo---react-amplify-serverless/todo/src/App.js
@@ -5,11 +5,15 @@ import { withAuthenticator } from '@aws-amplify/ui-react'
 import Amplify, { Auth, API, graphqlOperation } from 'aws-amplify';
 import awsconfig from './aws-exports';
 import * as queries from './graphql/queries'
+import * as mutations from './graphql/mutations'
+import * as subscriptions from './graphql/subscriptions'
 
 Amplify.configure(awsconfig);
 
 function App() {
   const [todos, setTodos] = useState([])
+  const [newTodo, setNewTodo] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     const fetchTodos = async () => {
@@ -20,18 +24,61 @@ function App() {
     fetchTodos()
   }, [])
 
+  useEffect(() => {
+    const getUser = async () => {
+      const user = await Auth.currentUserInfo();
+      setUser(user)
+      return user;
+    };
+
+    getUser()
+  }, [])
+
+  useEffect(() => {
+    if (user) {
+      const onCreateTodoSubscription = API.graphql(graphqlOperation(subscriptions.onCreateTodo, { owner: user.username }))
+        .subscribe({
+          next: (result) => {
+            const items = [...todos]
+            items.push(result.value.data.onCreateTodo)
+            setTodos(items)
+          }
+        })
+      const onUpdateTodoSubscription = API.graphql(graphqlOperation(subscriptions.onUpdateTodo, { owner: user.username }))
+        .subscribe({
+          next: (result) => {
+            const items = [...todos]
+            const todo = result.value.data.onUpdateTodo
+            const idx = items.findIndex((itm) => itm.id === todo.id);
+            items[idx] = todo
+            setTodos(items)
+          }
+        })
+      
+      return () => {
+        onCreateTodoSubscription.unsubscribe()
+        onUpdateTodoSubscription.unsubscribe()
+      }
+    }
+  }, [user, todos])
+
   const logout = () => {
     Auth.signOut()
     window.location.reload()
   }
-
-  console.log(todos)
+  const addTodo = async () => {
+    const createTodoInput = { input: { name: newTodo, status: 'NEW' }}
+    await API.graphql(graphqlOperation(mutations.createTodo, createTodoInput))
+    setNewTodo('')
+  }
 
   return (
     <div className="App">
       <main>
         <h1>TODO List</h1>
         <TodoList todos={todos} />
+        <input type="text" value={newTodo} onChange={(e) => setNewTodo(e.target.value)} />
+        <button onClick={addTodo}>Add new todo</button>
       </main>
       <button onClick={logout}>Log Out</button>
     </div>
--- todo---react-amplify-serverless/todo/src/components/TodoItem/TodoItem.js
+++ todo---react-amplify-serverless/todo/src/components/TodoItem/TodoItem.js
@@ -1,10 +1,23 @@
+import { API, graphqlOperation } from 'aws-amplify'
 import React from 'react'
+import * as mutations from '../../graphql/mutations'
 
 const TodoItem = ({ item }) => {
+  const updateTodo = async (e) => {
+    const todoStatus = e.target.checked ? 'DONE' : 'NOT DONE'
+    const updateTodoInput = {
+      input: {
+        id: item.id,
+        name: item.name,
+        status: todoStatus
+      }
+    }
+    await API.graphql(graphqlOperation(mutations.updateTodo, updateTodoInput))
+  }
   return (
     <li>
       <label>
-        <input type="checkbox"/>
+        <input type="checkbox" checked={item.status === 'DONE'} onChange={updateTodo} />
         <span>{item.name}</span>
       </label>
     </li>
